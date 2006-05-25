d79ff6e506f46507d4790da0f12604a3bf5b10f3
Sven Klemm <sven@c3d2.de>
2006-05-25 23:11:17 +0000

    r4387@pinealgland:  sven | 2006-05-26 01:05:19 +0200  further adjustments to benchmark

--- benchmark.rb
+++ benchmark.rb
@@ -11,7 +11,7 @@ include Benchmark
 
 GC.disable
 
-QUERY_REPEAT = 10
+QUERY_REPEAT = 2
 ITERATION_REPEAT = 10
 
 puts "==Momomoto==========================================================================="
@@ -25,9 +25,7 @@ Benchmark.benchmark(" "*40 + CAPTION, 40, FMTSTR, ">total:", ">avg:") do | b |
   end
 
   times << b.report("querying #{QUERY_REPEAT} times") do
-    QUERY_REPEAT.times do 
-      a = Momomoto::Information_schema::Columns.select
-    end
+    QUERY_REPEAT.times do a = Momomoto::Information_schema::Columns.select end
   end
 
   times << b.report("iterating #{ITERATION_REPEAT} times over the resultset") do
@@ -37,16 +35,13 @@ Benchmark.benchmark(" "*40 + CAPTION, 40, FMTSTR, ">total:", ">avg:") do | b |
   repeat = ITERATION_REPEAT * a.length
   times << b.report("accessing an element #{repeat} times") do
     a = a[0]
-    repeat.times do 
-      a.table_catalog
-    end
+    repeat.times do a.table_catalog end
   end
 
   times << b.report("querying #{QUERY_REPEAT} times for subset") do
-    QUERY_REPEAT.times do 
-      a = Momomoto::Information_schema::Columns.select({:table_name => 'columns'})
-    end
+    QUERY_REPEAT.times do a = Momomoto::Information_schema::Columns.select({:table_name => 'columns'}) end
   end
+
   total = times.inject do | memo, time | memo += time end
   [ total , total / times.length ]
 end
@@ -62,14 +57,10 @@ end
 Benchmark.benchmark(" "*40 + CAPTION, 40, FMTSTR, ">total:", ">avg:") do | b |
   a = nil
   times = []  
-  times << b.report("querying the first time") do
-    a = Columns.find_all
-  end
+  times << b.report("querying the first time") do a = Columns.find_all end
   
   times << b.report("querying #{QUERY_REPEAT} times") do
-    QUERY_REPEAT.times do
-      a = Columns.find_all
-    end
+    QUERY_REPEAT.times do a = Columns.find_all end
   end
 
   times << b.report("iterating #{ITERATION_REPEAT} times over the resultset") do
@@ -79,15 +70,11 @@ Benchmark.benchmark(" "*40 + CAPTION, 40, FMTSTR, ">total:", ">avg:") do | b |
   repeat = ITERATION_REPEAT * a.length
   times << b.report("accessing an element #{repeat} times") do
     a = a[0]
-    repeat.times do 
-      a.table_catalog
-    end
+    repeat.times do a.table_catalog end
   end
 
   times << b.report("querying #{QUERY_REPEAT} times for subset") do
-    QUERY_REPEAT.times do 
-      a = Columns.find_all_by_table_name('columns')
-    end
+    QUERY_REPEAT.times do a = Columns.find_all_by_table_name('columns') end
   end
 
   total = times.inject do | memo, time | memo += time end
@@ -102,13 +89,11 @@ Benchmark.benchmark(" "*40 + CAPTION, 40, FMTSTR, ">total:", ">avg:") do | b |
   a = nil
   times = []  
   times << b.report("querying the first time") do
-    a = conn.exec("SELECT * FROM information_schema.columns;").entries
+    a = conn.exec("SELECT table_catalog, table_schema, table_name, column_name, ordinal_position, column_default, is_nullable, data_type, character_maximum_length, character_octet_length, numeric_precision, numeric_precision_radix, numeric_scale, datetime_precision, interval_type, interval_precision, character_set_catalog, character_set_schema, character_set_name, collation_catalog, collation_schema, collation_name, domain_catalog, domain_schema, domain_name, udt_catalog, udt_schema, udt_name, scope_catalog, scope_schema, scope_name, maximum_cardinality, dtd_identifier, is_self_referencing FROM information_schema.columns;").entries
   end
   
   times << b.report("querying #{QUERY_REPEAT} times") do
-    QUERY_REPEAT.times do
-      a = conn.exec("SELECT * FROM information_schema.columns;").entries
-    end
+    QUERY_REPEAT.times do a = conn.exec("SELECT * FROM information_schema.columns;").entries end
   end
     
   times << b.report("iterating #{ITERATION_REPEAT} times over the resultset") do
@@ -118,9 +103,7 @@ Benchmark.benchmark(" "*40 + CAPTION, 40, FMTSTR, ">total:", ">avg:") do | b |
   repeat = ITERATION_REPEAT * a.length
   times << b.report("accessing an element #{repeat} times") do
     a = a[0]
-    repeat.times do 
-      a[0]
-    end
+    repeat.times do a[0] end
   end
 
   times << b.report("querying #{QUERY_REPEAT} times for subset") do
