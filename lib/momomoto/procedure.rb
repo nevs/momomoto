81eea6989f50e4ccd1b0592a9d86ba7633841a9b
Sven Klemm <sven@c3d2.de>
2006-05-30 21:06:21 +0000

    r4487@pinealgland:  sven | 2006-05-30 23:00:07 +0200  added functions for setting parameter of procedures

--- lib/momomoto/procedure.rb
+++ lib/momomoto/procedure.rb
@@ -5,24 +5,58 @@ module Momomoto
   # it must not be used directly but you should inherit from this class
   class Procedure < Base
 
-    # guesses the procedure name of the procedure this class works on
-    def self.construct_procedure_name( classname ) # :nodoc:
-      classname.split('::').last.downcase.gsub(/[^a-z_0-9]/, '')
-    end
+    class << self
 
-    # set the procedure_name of the table this class operates on
-    def self.procedure_name=( procedure_name )
-      class_variable_set( :@@procedure_name, procedure_name )
-    end
-
-    # get the procedure_name of the table this class operates on
-    def self.procedure_name( procedure_name = nil )
-      return self.procedure_name=( procedure_name ) if procedure_name
-      begin
-        class_variable_get( :@@procedure_name )
-      rescue NameError
-        construct_procedure_name( self.name )
+      # guesses the procedure name of the procedure this class works on
+      def construct_procedure_name( classname ) # :nodoc:
+        classname.split('::').last.downcase.gsub(/[^a-z_0-9]/, '')
       end
+
+      # set the procedure name
+      def procedure_name=( procedure_name )
+        class_variable_set( :@@procedure_name, procedure_name )
+      end
+
+      # get the procedure name
+      def procedure_name( procedure_name = nil )
+        return self.procedure_name=( procedure_name ) if procedure_name
+        begin
+          class_variable_get( :@@procedure_name )
+        rescue NameError
+          construct_procedure_name( self.name )
+        end
+      end
+
+      # set the parameter this procedures accepts
+      def parameter=( parameter )
+        class_variable_set( :@@parameter, parameter)
+      end
+
+      # get the parameter this procedure accepts
+      def parameter( parameter = nil )
+        return self.parameter=( parameter ) if parameter
+        begin
+          class_variable_get( :@@parameter )
+        rescue NameError
+          nil
+        end
+      end
+
+      # set the columns of this procedure returns 
+      def columns=( columns )
+        class_variable_set( :@@columns, columns)
+      end
+
+      # get the columns of this procedure returns
+      def columns( columns = nil )
+        return self.columns=( columns ) if columns
+        begin
+          class_variable_get( :@@columns )
+        rescue NameError
+          nil
+        end
+      end
+
     end
 
   end
--- test/test_procedure.rb
+++ test/test_procedure.rb
@@ -1,18 +1,41 @@
 
 class TestProcedure < Test::Unit::TestCase
 
-  def test_procedure_name_getter
+  def test_procedure_name
     self.class.const_set( :Proc1, Class.new( Momomoto::Procedure ) )
     assert_equal( 'proc1', Proc1.procedure_name )
-  end
-
-  def test_procedure_name_setter
     self.class.const_set( :Proc2, Class.new( Momomoto::Procedure ) )
     assert_equal( 'proc2', Proc2.procedure_name )
     Proc2.procedure_name( 'proc3' )
     assert_equal( 'proc3', Proc2.procedure_name )
     Proc2.procedure_name = 'proc4'
     assert_equal( 'proc4', Proc2.procedure_name )
+    assert_equal( 'proc1', Proc1.procedure_name )
+  end
+
+  def test_columns
+    p = Class.new( Momomoto::Procedure )
+    p2 = Class.new( Momomoto::Procedure )
+    p.columns = :chunky
+    p2.columns = :alice
+    assert_equal( :chunky, p.columns )
+    p.columns = :bacon
+    assert_equal( :bacon, p.columns )
+    p.columns( :chunky )
+    assert_equal( :chunky, p.columns )
+    assert_equal( :alice, p2.columns )
+  end
+
+  def test_parameter
+    p = Class.new( Momomoto::Procedure )
+    p2 = Class.new( Momomoto::Procedure )
+    p.parameter = :chunky
+    p2.parameter = :bacon
+    assert_equal( :chunky, p.parameter )
+    assert_equal( :bacon, p2.parameter )
+    p.parameter( :alice )
+    assert_equal( :alice, p.parameter )
+    assert_equal( :bacon, p2.parameter )
   end
 
 end
