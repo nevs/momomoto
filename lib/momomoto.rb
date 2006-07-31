ac654498ec3a84d81aa5827e283589105f079b36
Sven Klemm <sven@c3d2.de>
2006-07-31 20:17:14 +0000

    r4825@pinealgland:  sven | 2006-07-31 22:09:31 +0200

--- lib/momomoto.rb
+++ lib/momomoto.rb
@@ -1,11 +1,5 @@
 
 $LOAD_PATH.unshift( File.dirname( __FILE__ ) )
 
-require 'momomoto/base'
-require 'momomoto/table'
-require 'momomoto/join'
-require 'momomoto/procedure'
-require 'momomoto/order'
-require 'momomoto/row'
-require 'momomoto/datatype'
-require 'momomoto/database'
+require 'momomoto/momomoto'
+
---
+++ lib/momomoto/momomoto.rb
@@ -0,0 +1,10 @@
+
+require 'momomoto/base'
+require 'momomoto/table'
+require 'momomoto/join'
+require 'momomoto/procedure'
+require 'momomoto/order'
+require 'momomoto/row'
+require 'momomoto/datatype'
+require 'momomoto/database'
+
