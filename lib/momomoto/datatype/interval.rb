02e3ea97bfc19dad8e44d9afc4c635e120d8de45
Sven Klemm <sven@c3d2.de>
2006-08-31 12:08:10 +0000

    r5568@pinealgland:  sven | 2006-08-31 14:04:39 +0200  workaround for interval

--- lib/momomoto/datatype/interval.rb
+++ lib/momomoto/datatype/interval.rb
@@ -1,7 +1,15 @@
 module Momomoto
   module Datatype
     class Interval < Time_without_time_zone
-    
+
+      def escape( value )
+        case value
+          when nil then 'NULL'
+          when String then "'#{Database.escape_string(value)}'"
+          else "'#{Database.escape_string(value.strftime('%H:%M:%S'))}'"
+        end
+      end
+
       def filter_get( value )
         case value
           when nil, '' then nil
