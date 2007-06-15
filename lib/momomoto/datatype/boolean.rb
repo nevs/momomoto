e3f8708222d6b026a3a74755856bbcf64d5ac865
Sven Klemm <sven@c3d2.de>
2007-06-15 08:30:09 +0000

    adding on and off to recognized values for boolean datatype

--- lib/momomoto/datatype/boolean.rb
+++ lib/momomoto/datatype/boolean.rb
@@ -4,16 +4,16 @@ module Momomoto
 
       def filter_set( value )
         case value
-          when true, 1, 't', 'true' then true
-          when false, 0, 'f', 'false' then false
+          when true, 1, 't', 'true', 'on' then true
+          when false, 0, 'f', 'false', 'off' then false
           else not_null? ? false : nil
         end
       end
 
       def escape( input )
         case input
-          when true, 1, 't', 'true' then "'t'"
-          when false, 0, 'f', 'false' then "'f'"
+          when true, 1, 't', 'true', 'on' then "'t'"
+          when false, 0, 'f', 'false', 'off' then "'f'"
           else "NULL"
         end
       end
