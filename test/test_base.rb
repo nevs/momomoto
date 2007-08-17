c30e1f55c59ff3f61227efa29767011a4484865b
Sven Klemm <sven@c3d2.de>
2007-08-17 10:19:32 +0000

    minor test fix

--- test/test_base.rb
+++ test/test_base.rb
@@ -15,20 +15,20 @@ class TestBase < Test::Unit::TestCase
     assert_equal( "AND", t2.logical_operator )
     t1.logical_operator = "or"
     assert_equal( "OR", t1.logical_operator )
-    assert_equal( " WHERE first_name = E'a' OR person_id = '1'" , t1.compile_where(:person_id=>'1',:first_name=>'a') )
-    assert_equal( " WHERE first_name = E'a' AND person_id = '1'" , t2.compile_where(:person_id=>'1',:first_name=>'a') )
+    assert_equal( " WHERE first_name = E'a' OR person_id = '1'" , t1.instance_eval do compile_where(:person_id=>'1',:first_name=>'a') end )
+    assert_equal( " WHERE first_name = E'a' AND person_id = '1'" , t2.instance_eval do compile_where(:person_id=>'1',:first_name=>'a') end )
   end
 
   def test_compile_where
     t = Class.new( Momomoto::Table )
     t.table_name = 'person'
     t.initialize_table
-    assert_equal( " WHERE person_id = '1'" , t.compile_where( :person_id => '1' ) )
-    assert_equal( " WHERE person_id IN ('1')" , t.compile_where( :person_id => ['1'] ) )
-    assert_equal( " WHERE person_id IN ('1','2')" , t.compile_where( :person_id => ['1',2] ) )
-    assert_equal( " WHERE first_name = E'1'" , t.compile_where( :first_name => '1' ) )
-    assert_equal( " WHERE first_name = E'chu''nky'" , t.compile_where( :first_name => "chu'nky" ) )
-    assert_equal( " WHERE first_name IN (E'chu''nky',E'bac''n')" , t.compile_where( :first_name => ["chu'nky","bac'n"] ) )
+      assert_equal( " WHERE person_id = '1'" , t.instance_eval do compile_where( :person_id => '1' ) end )
+      assert_equal( " WHERE person_id IN ('1')" , t.instance_eval do compile_where( :person_id => ['1'] ) end )
+      assert_equal( " WHERE person_id IN ('1','2')" , t.instance_eval do compile_where( :person_id => ['1',2] ) end )
+      assert_equal( " WHERE first_name = E'1'" , t.instance_eval do compile_where( :first_name => '1' ) end )
+      assert_equal( " WHERE first_name = E'chu''nky'" , t.instance_eval do compile_where( :first_name => "chu'nky" ) end )
+      assert_equal( " WHERE first_name IN (E'chu''nky',E'bac''n')" , t.instance_eval do compile_where( :first_name => ["chu'nky","bac'n"] ) end )
   end
 
 end
