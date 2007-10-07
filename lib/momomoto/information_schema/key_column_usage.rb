26a31053f72022f3c43bc12f302d85f22b652904
Sven Klemm <sven@c3d2.de>
2007-10-07 18:51:50 +0000

    remove columns from information_schema::key_column_usage

--- lib/momomoto/information_schema/key_column_usage.rb
+++ lib/momomoto/information_schema/key_column_usage.rb
@@ -2,17 +2,7 @@
 module Momomoto
   module Information_schema
     class Key_column_usage < Momomoto::Table
-      primary_keys( [] )
       schema_name( "information_schema" )
-      columns( { :constraint_catalog        => Momomoto::Datatype::Character_varying.new,
-                 :constraint_schema         => Momomoto::Datatype::Character_varying.new,
-                 :constraint_name           => Momomoto::Datatype::Character_varying.new,
-                 :table_catalog             => Momomoto::Datatype::Character_varying.new,
-                 :table_schema              => Momomoto::Datatype::Character_varying.new,
-                 :table_name                => Momomoto::Datatype::Character_varying.new,
-                 :column_name               => Momomoto::Datatype::Character_varying.new,
-                 :ordinal_position          => Momomoto::Datatype::Integer.new
-               } )
     end
   end
 end
