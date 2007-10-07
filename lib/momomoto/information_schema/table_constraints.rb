b599a65421e08e55f1b74517f062e704bfaf22bf
Sven Klemm <sven@c3d2.de>
2007-10-07 18:51:23 +0000

    remove columns from Information_schema::Table_constraints because introspection can fetch them

--- lib/momomoto/information_schema/table_constraints.rb
+++ lib/momomoto/information_schema/table_constraints.rb
@@ -4,16 +4,6 @@ module Momomoto
     class Table_constraints < Momomoto::Table
       primary_keys( [] )
       schema_name( "information_schema" )
-      columns( { :constraint_catalog        => Momomoto::Datatype::Text.new,
-                 :constraint_schema         => Momomoto::Datatype::Text.new,
-                 :constraint_name           => Momomoto::Datatype::Text.new,
-                 :table_catalog             => Momomoto::Datatype::Text.new,
-                 :table_schema              => Momomoto::Datatype::Text.new,
-                 :table_name                => Momomoto::Datatype::Text.new,
-                 :constraint_type           => Momomoto::Datatype::Text.new,
-                 :is_deferrable             => Momomoto::Datatype::Text.new,
-                 :initially_deferred        => Momomoto::Datatype::Text.new
-               } )
     end
   end
 end
