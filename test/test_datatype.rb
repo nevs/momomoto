78f797cf426df8789db776eae7803e5d154db73f
Sven Klemm <sven@c3d2.de>
2007-10-13 18:43:26 +0000

    optimize test cases

--- test/test_datatype.rb
+++ test/test_datatype.rb
@@ -1,22 +1,18 @@
 
 class TestDatatype < Test::Unit::TestCase
 
-  DATATYPES = Momomoto::Datatype.constants.map do | t | 
-    Momomoto::Datatype.const_get( t ) 
-  end
+  Momomoto::Datatype.constants.each do | type_name | 
+    type = Momomoto::Datatype.const_get( type_name ) 
 
-  def test_default
-    DATATYPES.each do | type |
+    define_method( "test_default_#{type_name}" ) do
       row = Momomoto::Information_schema::Columns.new
       row.column_default = 'default-value'
       a = type.new( row )
       assert_equal( 'default-value', a.default )
       assert_equal( false, type.new.default )
     end
-  end
 
-  def test_not_null
-    DATATYPES.each do | type |
+    define_method( "test_not_null_#{type_name}" ) do
       row = Momomoto::Information_schema::Columns.new
       row.is_nullable = "YES"
       assert_equal( false, type.new( row ).not_null? )
@@ -25,10 +21,8 @@ class TestDatatype < Test::Unit::TestCase
       row.is_nullable = nil
       assert_equal( false, type.new( row ).not_null? )
     end
-  end
 
-  def test_compile_rule
-    DATATYPES.each do | type |
+    define_method( "test_compile_rule_#{type_name}" ) do
       t = type.new
       tests = [ nil, [], [nil], [nil,nil], {}, {:eq=>nil}, {nil=>nil}]
       tests.each do | test |
@@ -37,18 +31,14 @@ class TestDatatype < Test::Unit::TestCase
         end
       end
     end
-  end
 
-  def test_compile_rule_null
-    DATATYPES.each do | type |
+    define_method( "test_compile_rule_null_#{type_name}" ) do
       t = type.new
       assert_equal( "field_name IS NULL", t.compile_rule( :field_name, :NULL) )
       assert_equal( "field_name IS NOT NULL", t.compile_rule( :field_name, :NOT_NULL) )
     end
-  end
 
-  def test_operator_sign
-    DATATYPES.each do | type |
+    define_method( "test_operator_sign_#{type_name}" ) do
       assert_equal( '<=', type.operator_sign( :le ) )
       assert_equal( '<', type.operator_sign( :lt ) )
       assert_equal( '>=', type.operator_sign( :ge ) )
@@ -57,10 +47,8 @@ class TestDatatype < Test::Unit::TestCase
       assert_equal( '<>', type.operator_sign( :ne ) )
       assert_raise( Momomoto::CriticalError ) { type.operator_sign( :foo ) }
     end
-  end
 
-  def test_escape
-    DATATYPES.each do | type |
+    define_method( "test_escape_#{type_name}" ) do
       t = type.new
       assert_equal( 'NULL', t.escape( nil ))
     end
