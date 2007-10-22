bfb82e623b073dc9bb5f39a3628f6452e02f4868
Sven Klemm <sven@c3d2.de>
2007-10-22 13:20:34 +0000

    use TimeInterval for INTERVAL datatypes

--- lib/momomoto/datatype/interval.rb
+++ lib/momomoto/datatype/interval.rb
@@ -13,8 +13,8 @@ module Momomoto
       def filter_get( value )
         case value
           when nil, '' then nil
-          when ::Time then value
-          when String then ::Time.parse( value )
+          when ::TimeInterval then value
+          when String then ::TimeInterval.parse( value )
           else raise Error
         end
        rescue => e
--- lib/momomoto/momomoto.rb
+++ lib/momomoto/momomoto.rb
@@ -4,6 +4,7 @@ require 'momomoto/table'
 require 'momomoto/procedure'
 require 'momomoto/order'
 require 'momomoto/row'
+require 'momomoto/timeinterval.rb'
 require 'momomoto/datatype'
 require 'momomoto/database'
 
