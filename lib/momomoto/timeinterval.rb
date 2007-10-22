0340c426ba36e6e099ed55293df365ab6c520e42
Sven Klemm <sven@c3d2.de>
2007-10-22 13:19:52 +0000

    adding TimeInterval implementation

---
+++ lib/momomoto/timeinterval.rb
@@ -0,0 +1,50 @@
+
+require 'date'
+
+class TimeInterval
+  
+  attr_reader :hour, :min, :sec
+
+  class << self
+
+    def parse( interval )
+      d = Date._parse( interval, false)
+      if d.empty? && interval.length > 0
+        raise "Could not parse interval `#{interval}`"
+      end
+      if !(d.keys - [:hour,:min,:sec]).empty?
+        raise "Could not parse interval `#{interval}`"
+      end
+      TimeInterval.new( d )
+    end
+
+  end
+
+  def strftime( fmt = "%H:%M:%S" )
+    fmt.gsub( /%(.)/ ) do | match |
+      case match[1,1]
+        when 'H' then sprintf('%02d',@hour)
+        when 'M' then sprintf('%02d',@min)
+        when 'S' then sprintf('%02d',@sec)
+        when '%' then '%'
+        else '%' + match
+      end
+    end
+  end
+
+  def to_i
+    @hour * 3600 + @min * 60 + @sec
+  end
+
+  def to_s
+    strftime
+  end
+
+  def initialize( d = {} )
+    @hour = d[:hour] || 0
+    @min = d[:min] || 0
+    @sec = d[:sec] || 0
+  end
+
+end
+
