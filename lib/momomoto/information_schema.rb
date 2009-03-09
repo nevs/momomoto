907078697e1b9363a9d7e722b03d5256d11b6d5b
Sven Klemm <sven@c3d2.de>
2009-03-09 22:59:11 +0000

    enhance information_schema module to first try to require

--- lib/momomoto/information_schema.rb
+++ lib/momomoto/information_schema.rb
@@ -2,12 +2,18 @@
 module Momomoto::Information_schema 
 
   def self.const_missing( table )
-    klass = Class.new( ::Momomoto::Table )
-    klass.schema_name = "information_schema"
-    klass.table_type = "VIEW"
-    klass.primary_keys( [] )
+    raise CriticalError, "Invalid name." unless table.to_s.downcase.match(/^[a-z0-9_]+$/)
+    begin
+      require "momomoto/information_schema/#{table.to_s.downcase}"
+      ::Momomoto::Information_schema::const_get( table )
+    rescue LoadError
+      klass = Class.new( ::Momomoto::Table )
+      klass.schema_name = "information_schema"
+      klass.table_type = "VIEW"
+      klass.primary_keys( [] )
 
-    ::Momomoto::Information_schema.const_set(table, klass)
+      ::Momomoto::Information_schema.const_set(table, klass)
+    end
   end
 
 end
